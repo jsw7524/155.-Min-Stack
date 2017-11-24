d705d228773817e5e8bac55f649200ac3b044451
Jason Wu <jsw7524@gmail.com>
2017-11-24 20:13:25 +0800

    c#

--- 155. Min Stack/Program.cs
+++ 155. Min Stack/Program.cs
@@ -6,6 +6,37 @@ using System.Threading.Tasks;
 
 namespace _155.Min_Stack
 {
+    public class MinStack
+    {
+        private Stack<int> Stack { set; get; }
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+            Stack = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            Stack.Push(x);
+        }
+
+        public void Pop()
+        {
+            Stack.Pop();
+        }
+
+        public int Top()
+        {
+            return Stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return Stack.Min();
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
